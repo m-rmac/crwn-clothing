9ceb0173d898131a0b32b215fc546564471483eb
Molly MacDonald <mmac428@aucklanduni.ac.nz>
2021-12-02 19:19:44 +1300

    update menu items to navigate withRouter

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -53,12 +53,11 @@ class Directory extends React.Component {
     render(){
         return(
             <div className='directory-menu'>
-                {
-                    this.state.sections.map(({title, imageUrl, id, size}) =>
+                {this.state.sections.map(({id, ...otherSectionProps}) => ( 
                         
-                        <MenuItem key = {id} title = {title} imageUrl = {imageUrl} size = {size}/>
+                        <MenuItem key = {id} {...otherSectionProps}/>
                         
-                        )
+                        ))
                 }
             </div>
         )
