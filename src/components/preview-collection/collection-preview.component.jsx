4cc4d241664035b33c03a1b1633f42f43d279ac0
Molly MacDonald <mmac428@aucklanduni.ac.nz>
2022-02-24 12:38:20 +0100

    updating app with a shop page, collectn preview component and collection component

---
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import './collection-preview.style.scss';
+import CollectionItem from "../collection-item/collection-item.component";
+
+const CollectionPreview = ({ title, items }) => (
+    <div className="collection-preview">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <div className="preview">
+{/* This will be re rendered very time the components is - time concern */}
+            {items
+                .filter((item, idx) => idx <4)
+                .map(({id, ...otherItemProps}) => (
+                    <CollectionItem key={id} {...otherItemProps}/>
+
+                ))}
+
+        </div>
+
+    </div>
+);
+
+export default CollectionPreview;
